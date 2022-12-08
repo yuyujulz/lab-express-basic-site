4ee281db346fb905a5b1674f8777e12b9dc92098
Julia Ganina <ganina.julia2@gmail.com>
2022-12-08 20:48:52 +0000

    started the project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/home', (request,response) =>  { 
+    response.sendFile(__dirname + '/views/home.html')
+    
+})
+
+app.listen(3003, () => console.log('connected'))
